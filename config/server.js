3425560e49277877d21a29fb59470b416821c7cf
MariaFernandaAguiar <abreuaguiar07@gmail.com>
2020-12-09 17:50:41 -0500

    Prova Web2

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f223f149ae50c2c3246141c6f080c633'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '985080c7-073e-40f7-88d4-a9faf87f814f'
+};
